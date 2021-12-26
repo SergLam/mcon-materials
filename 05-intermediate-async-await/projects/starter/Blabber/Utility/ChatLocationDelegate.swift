dd2c92ffd301a67d796944f4db9b25f9b9030f34
SergLam <lamcevs0@gmail.com>
2021-12-26 15:55:35 +0200

    Finish chapter 5

--- 05-intermediate-async-await/projects/starter/Blabber/Utility/ChatLocationDelegate.swift
+++ 05-intermediate-async-await/projects/starter/Blabber/Utility/ChatLocationDelegate.swift
@@ -33,5 +33,50 @@
 import Foundation
 import CoreLocation
 
-class ChatLocationDelegate: NSObject, CLLocationManagerDelegate {
+class ChatLocationDelegate: NSObject {
+  
+  typealias LocationContinuation = CheckedContinuation<CLLocation, Error>
+  
+  private var continuation: LocationContinuation?
+  private let manager = CLLocationManager()
+  
+  init(continuation: LocationContinuation) {
+    self.continuation = continuation
+    super.init()
+    manager.delegate = self
+    manager.requestWhenInUseAuthorization()
+  }
+  
+}
+
+// MARK: - CLLocationManagerDelegate
+extension ChatLocationDelegate: CLLocationManagerDelegate {
+  
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    switch manager.authorizationStatus {
+    case .notDetermined:
+      break
+    case .authorizedAlways, .authorizedWhenInUse:
+      manager.startUpdatingLocation()
+    default:
+      continuation?.resume(
+        throwing: "The app isn't authorized to use location data"
+      )
+      continuation = nil
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let location = locations.first else {
+      return
+    }
+    continuation?.resume(returning: location)
+    continuation = nil
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    continuation?.resume(throwing: error)
+    continuation = nil
+  }
+  
 }
